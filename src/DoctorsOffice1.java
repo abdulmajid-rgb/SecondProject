4d727eefa26d60671d404cebebd9c11d2e9b4549
Abdulmejid <your-github-email>
2020-06-16 09:53:24 -0400

    DoctorsOffice1

---
+++ src/DoctorsOffice1.java
@@ -0,0 +1,16 @@
+public class DoctorsOffice1 {
+    public static void main(String[] args) {
+
+        String patientName = "Stacey Manson";
+        boolean hasInsurance = false;
+        double temp = 102.2;
+        int numVisits = 5;
+
+        System.out.println("Patient Details:");
+        System.out.println("Name: " + patientName);
+        System.out.println("Patient has insurance: " + hasInsurance);
+        System.out.println("Body temprature: ");
+        System.out.println("My integer numberVisits is equal to" + (numVisits));
+    }
+
+    }
